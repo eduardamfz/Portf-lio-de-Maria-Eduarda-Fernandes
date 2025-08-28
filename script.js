6eb7309ed9d30ff49d2a3975f0be7dbdea2c7689
eduardamfz <eduardamf.dev@gmail.com>
2025-08-28 03:53:39 -0300

    script.js

--- script.js
+++ script.js
@@ -14,13 +14,12 @@ document.querySelectorAll('header nav ul li a, .btn-Contato a').forEach(link => 
 document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault(); 
 
-    // Pega os dados digitados
     const nome = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const celular = document.getElementById("celular").value;
     const mensagem = document.getElementById("message").value;
 
-    // Aqui usamos EmailJS (serviço gratuito para envio de e-mails sem backend)
+    // EmailJS 
     emailjs.send("service_3ooc8v9", "template_s77dnbh", {
         from_name: nome,
         from_email: email,
@@ -33,3 +32,4 @@ document.querySelector("form").addEventListener("submit", function (e) {
         alert("Erro ao enviar. Tente novamente.");
     });
 });
+
